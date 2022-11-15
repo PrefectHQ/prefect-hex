c498062a6b4318dd1904745cc8a0b6e889b34e37
ahuang11 <andrew.h@prefect.io>
2022-11-15 15:35:46 -0800

    Add flow

--- prefect_hex/project.py
+++ prefect_hex/project.py
@@ -3,11 +3,17 @@ This is a module containing tasks for interacting with
 Hex projects
 """
 
-from typing import Dict, Optional
+import asyncio
+from typing import Dict, Optional, Tuple
 
-from prefect import task
+from prefect import flow, get_run_logger, task
 
 from prefect_hex import HexCredentials
+from prefect_hex.exceptions import (
+    TERMINAL_STATUS_EXCEPTIONS,
+    HexProjectRunError,
+    HexProjectRunTimedOut,
+)
 from prefect_hex.models import project as models
 from prefect_hex.rest import HTTPMethod, _unpack_contents, execute_endpoint
 
@@ -164,3 +170,175 @@ async def get_project_runs(
 
     contents = _unpack_contents(response)
     return models.ProjectRunsResponsePayload.parse_obj(contents)
+
+
+@flow
+async def trigger_project_run_and_wait_for_completion(
+    project_id: str,
+    hex_credentials: HexCredentials,
+    input_params: Optional[Dict] = None,
+    update_cache: bool = False,
+    max_wait_seconds: int = 900,
+    poll_frequency_seconds: int = 10,
+) -> models.ProjectRunResponsePayload:
+    """
+    Flow that triggers a project run and waits for the triggered run to complete.
+
+    Args:
+        project_id:
+            Project ID to run.
+        hex_credentials:
+            Credentials to use for authentication with Hex.
+        input_params:
+            Optional input parameter value map for this project run, e.g.
+            ```
+            {"text_input_1": "Hello World", "numeric_input_1": 123}
+            ```
+        update_cache:
+            When true, this run will update the cached state of the published app
+            with the latest run results. Additionally, any SQL cells
+            that have caching enabled will be re-executed as part of
+            this run. Note that this cannot be set to true if custom
+            input parameters are provided.
+        max_wait_seconds: Maximum number of seconds to wait for the entire
+            flow to complete.
+        poll_frequency_seconds: Number of seconds to wait in between checks for
+            run completion.
+
+    Returns:
+        Information about the triggered project run.
+
+    Examples:
+        Trigger a Hex project run and wait for completion as a stand alone flow.
+        ```python
+        import asyncio
+        from prefect_hex import HexCredentials
+        from prefect_hex.project import trigger_project_run_and_wait_for_completion
+
+        asyncio.run(
+            trigger_sync_run_and_wait_for_completion(
+                hex_credentials=HexCredentials(
+                    token="1abc0d23-1234-1a2b-abc3-12ab456c7d8e"
+                ),
+                project_id="012345c6-b67c-1234-1b2c-66e4ad07b9f3",
+                max_wait_seconds=1800,
+                poll_frequency_seconds=5,
+            )
+        )
+        ```
+
+        Trigger a Hex project run and wait for completion as a subflow.
+        ```python
+        from prefect import flow
+        from prefect_hex import HexCredentials
+        from prefect_hex.project import trigger_project_run_and_wait_for_completion
+
+        @flow
+        def trigger_project_run_and_wait_for_completion_flow():
+            project_id = "012345c6-b67c-1234-1b2c-66e4ad07b9f3"
+            hex_credentials = HexCredentials.load("hex-token")
+            project_status = trigger_project_run_and_wait_for_completion(
+                project_id=project_id,
+                hex_credentials=hex_credentials
+            )
+            return project_status
+
+        trigger_project_run_and_wait_for_completion_flow()
+        ```
+    """
+    logger = get_run_logger()
+
+    project_run_future = await run_project.submit(
+        project_id=project_id,
+        hex_credentials=hex_credentials,
+        input_params=input_params,
+        update_cache=update_cache,
+    )
+    project_run = await project_run_future.result()
+    run_id = project_run.run_id
+
+    logger.info(
+        "Started project %s run %s; visit %s to view the run.",
+        repr(project_id),
+        repr(run_id),
+        str(project_run.run_status_url),
+    )
+
+    project_status, project_metadata = await wait_for_project_run_completion(
+        project_id=project_id,
+        run_id=run_id,
+        hex_credentials=hex_credentials,
+        max_wait_seconds=max_wait_seconds,
+        poll_frequency_seconds=poll_frequency_seconds,
+    )
+
+    if project_status == models.ProjectRunStatus.completed:
+        return project_metadata
+    else:
+        raise TERMINAL_STATUS_EXCEPTIONS.get(project_status, HexProjectRunError)(
+            f"Sync ({project_metadata.slug!r}, ID {project_id!r}) "
+            f"was unsuccessful with {project_status.value!r} status"
+        )
+
+
+@flow
+async def wait_for_project_run_completion(
+    project_id: str,
+    run_id: str,
+    hex_credentials: HexCredentials,
+    max_wait_seconds: int = 900,
+    poll_frequency_seconds: int = 10,
+) -> Tuple[models.ProjectRunStatus, models.ProjectStatusResponsePayload]:
+    """
+    Flow that waits for the triggered project run to complete.
+
+    Args:
+        project_id:
+            Project ID to watch.
+        run_id:
+            Run ID to wait for.
+        hex_credentials:
+            Credentials to use for authentication with Hex.
+        max_wait_seconds: Maximum number of seconds to wait for the entire
+            flow to complete.
+        poll_frequency_seconds: Number of seconds to wait in between checks for
+            run completion.
+
+    Returns:
+        The status of the project run and the metadata associated with the run.
+
+    Examples:
+        ...
+    """
+    logger = get_run_logger()
+    seconds_waited_for_run_completion = 0
+    wait_for = []
+
+    while seconds_waited_for_run_completion <= max_wait_seconds:
+        project_future = await get_run_status.submit(
+            project_id=project_id,
+            run_id=run_id,
+            hex_credentials=hex_credentials,
+            wait_for=wait_for,
+        )
+        wait_for = [project_future]
+
+        project_metadata = await project_future.result()
+        project_status = project_metadata.status
+        if project_status in TERMINAL_STATUS_EXCEPTIONS.keys():
+            return project_status, project_metadata
+
+        logger.debug(
+            "Waiting on project %s run %s with sync status %s for %s seconds",
+            repr(project_id),
+            repr(run_id),
+            repr(project_status.value),
+            poll_frequency_seconds,
+        )
+        await asyncio.sleep(poll_frequency_seconds)
+        seconds_waited_for_run_completion += poll_frequency_seconds
+
+    raise HexProjectRunTimedOut(
+        f"Max wait time of {max_wait_seconds} seconds exceeded while waiting "
+        f"for project {project_id!r} run {run_id!r}"
+    )
